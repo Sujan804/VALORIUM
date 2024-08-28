f5123952b34350d756803bec3c0ab1235af484df
Sujan804 <sujanmec804@gmail.com>
2024-08-28 23:11:44 +0600

    progress: webview added, UI changed

---
+++ VALORIUM/Views/MainWindow/MainWindow.Toolbar.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace VALORIUM
+{
+    public partial class MainWindow
+    {
+        private void SetToolbarVisibility(bool isVisible)
+        {
+            if (isVisible)
+            {
+                toolBar.Visibility = Visibility.Visible;
+                menuItemHideToolbar.Header = "_Hide toolbar";
+
+            }
+            else
+            {
+                toolBar.Visibility = Visibility.Collapsed;
+                menuItemHideToolbar.Header = "_Show toolbar";
+            }
+        }
+        private void TogglePanel_Click(object sender, RoutedEventArgs e)
+        {
+            isSidePanelVisible = !isSidePanelVisible;
+            SetSidePanelVisibility(isSidePanelVisible);
+        }
+        private void SetSidePanelVisibility(bool isVisible)
+        {
+            if (isVisible)
+            {
+                pnlRight.Visibility = Visibility.Visible;
+                menuItemHideSidebar.Header = "_Hide sidebar";
+                RightPanelColumn.Width = new GridLength(0.2, GridUnitType.Star); // 20% of the width
+
+            }
+            else
+            {
+                pnlRight.Visibility = Visibility.Collapsed;
+                menuItemHideSidebar.Header = "_Show sidebar";
+                RightPanelColumn.Width = new GridLength(0); // Collapse the column
+            }
+        }
+        private void MenuItem_HideToolbar_Click(object sender, RoutedEventArgs e)
+        {
+            isToolbarVisible = !isToolbarVisible;
+            SetToolbarVisibility(isToolbarVisible);
+        }
+        private void btnCancelWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnMinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ VALORIUM/Views/MainWindow/MainWindow.Web.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VALORIUM
+{
+    public partial class MainWindow
+    {
+       
+
+        // Load the file into the WebView2 control
+        
+        void LoadWebView()
+        {
+            string htmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "Web", "index.html");
+            webView.Source = new Uri(htmlFilePath);
+        }
+    }
+}
